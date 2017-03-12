a3cdd7f71281356c5e59b1830505dddbaf20a16a
Julia <berrylily.0709@gmail.com>
2017-03-12 00:40:42 -0800

    change charts

--- js/d3js.my.js
+++ js/d3js.my.js
@@ -1,35 +1,124 @@
-var data = [90, 95, 100, 76, 60, 62, 91, 72, 53, 57, 89];
-var r = 300;
+// h1
+d3.select("h1").transition()
+    .ease(d3.easeLinear)
+    .duration(2000)
+    .style("color", "#FF556C");
 
+// margin
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = 500 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom,
+    radius = width/2;
+
+// color range
 var color = d3.scaleOrdinal()
-    .range(["#e60012", "#f39800", "#fff100", "#8fc31f", "#009944", "#009e96", "#00a0e9", "#0068b7", "#1d2088", "#920783", "#e4007f"]);
+    .range(["#FF556C", "#FFB42A", "#FFEA2A", "#D4FF2A", "#55FFB3", "#55DAFF", "#5566FF", "#FF55D2"]);
 
-var canvas = d3.select("body").append("svg")
-    .attr("width",1200)
-    .attr("height", 1200);
-
-var group = canvas.append("g")
-    .attr("transform", "translate(300, 300)");
-
+// pie chart arc. Need to create arcs before generating pie
 var arc = d3.arc()
-    .innerRadius(180)
-    .outerRadius(r);
+    .outerRadius(radius - 10)
+    .innerRadius(0);
 
+// donut chart arc
+var arc2 = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+// arc for the labels position
+var labelArc = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+// generate pie chart and donut chart
 var pie = d3.pie()
-    .value(function (d) {return d; });
+    .sort(null)
+    .value(function(d) { return d.count; });
 
-var arcs =group.selectAll(".arc")
-    .data(pie(data))
-    .enter()
-    .append("g")
-    .attr("class", "arc");
+// define the svg for pie chart
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-arcs.append("path")
-    .attr("d", arc)
-    .attr("fill", function (d) { return color(d.data); });
+// define the svg donut chart
+var svg2 = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-arcs.append("text")
-    .attr("transform", function (d) {return "translate(" + arc.centroid(d) + ")"})
-    .attr("text-anchor", "middle")
-    .attr("text-size", "1.5em")
-    .text(function (d) { return d.data; });
+// import data
+d3.csv("data.csv", function(error, data) {
+  if (error) throw error;
+
+    // parse data
+    data.forEach(function(d) {
+        d.count = +d.count;
+        d.fruit = d.fruit;
+    })
+
+  // "g element is a container used to group other SVG elements"
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  // append path
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.fruit); })
+    // transition
+    .transition()
+      .ease(d3.easeLinear)
+      .duration(2000)
+      .attrTween("d", tweenPie);
+
+  // append text
+  g.append("text")
+    .transition()
+      .ease(d3.easeLinear)
+      .duration(2000)
+    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.fruit; });
+
+
+    // "g element is a container used to group other SVG elements"
+  var g2 = svg2.selectAll(".arc2")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc2");
+
+   // append path
+  g2.append("path")
+      .attr("d", arc2)
+      .style("fill", function(d) { return color(d.data.fruit); })
+    .transition()
+      .ease(d3.easeLinear)
+      .duration(2000)
+      .attrTween("d", tweenDonut);
+
+   // append text
+  g2.append("text")
+    .transition()
+      .ease(d3.easeLinear)
+      .duration(2000)
+    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.fruit; });
+
+});
+
+// Helper function for animation of pie chart and donut chart
+function tweenPie(b) {
+  b.innerRadius = 0;
+  var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+  return function(t) { return arc(i(t)); };
+}
+
+function tweenDonut(b) {
+  b.innerRadius = 0;
+  var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+  return function(t) { return arc2(i(t)); };
+}
