ff14f1005977358887f0899411a2731c808c7ee9
Julia <berrylily.0709@gmail.com>
2017-03-12 15:13:19 -0700

    align paragraphs

--- js/d3js.my.js
+++ js/d3js.my.js
@@ -1,52 +1,52 @@
 // h1
 d3.select("h1").transition()
-    .ease(d3.easeLinear)
-    .duration(2000)
-    .style("color", "#FF556C");
+  .ease(d3.easeLinear)
+  .duration(2000)
+  .style("color", "#FF556C");
 
 // margin
 var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 500 - margin.right - margin.left,
-    height = 500 - margin.top - margin.bottom,
-    radius = width/2;
+  width = 500 - margin.right - margin.left,
+  height = 500 - margin.top - margin.bottom,
+  radius = width/2;
 
 // color range
 var color = d3.scaleOrdinal()
-    .range(["#FF556C", "#FFB42A", "#FFEA2A", "#D4FF2A", "#55FFB3", "#55DAFF", "#5566FF", "#FF55D2"]);
+  .range(["#FF556C", "#FFB42A", "#FFEA2A", "#D4FF2A", "#55FFB3", "#55DAFF", "#5566FF", "#FF55D2"]);
 
 // pie chart arc. Need to create arcs before generating pie
 var arc = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+  .outerRadius(radius - 10)
+  .innerRadius(0);
 
 // donut chart arc
 var arc2 = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
+  .outerRadius(radius - 10)
+  .innerRadius(radius - 70);
 
 // arc for the labels position
 var labelArc = d3.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
+  .outerRadius(radius - 40)
+  .innerRadius(radius - 40);
 
 // generate pie chart and donut chart
 var pie = d3.pie()
-    .sort(null)
-    .value(function(d) { return d.count; });
+  .sort(null)
+  .value(function(d) { return d.count; });
 
 // define the svg for pie chart
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+  .attr("width", width)
+  .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // define the svg donut chart
 var svg2 = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+  .attr("width", width)
+  .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // import data
 d3.csv("data.csv", function(error, data) {
@@ -54,59 +54,59 @@ d3.csv("data.csv", function(error, data) {
 
     // parse data
     data.forEach(function(d) {
-        d.count = +d.count;
-        d.fruit = d.fruit;
+      d.count = +d.count;
+      d.fruit = d.fruit;
     })
 
   // "g element is a container used to group other SVG elements"
   var g = svg.selectAll(".arc")
-      .data(pie(data))
+    .data(pie(data))
     .enter().append("g")
-      .attr("class", "arc");
+    .attr("class", "arc");
 
   // append path
   g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.fruit); })
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data.fruit); })
     // transition
     .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .attrTween("d", tweenPie);
+    .ease(d3.easeLinear)
+    .duration(2000)
+    .attrTween("d", tweenPie);
 
   // append text
   g.append("text")
     .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
+    .ease(d3.easeLinear)
+    .duration(2000)
     .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.fruit; });
+    .attr("dy", ".35em")
+    .text(function(d) { return d.data.fruit; });
 
 
     // "g element is a container used to group other SVG elements"
   var g2 = svg2.selectAll(".arc2")
-      .data(pie(data))
+    .data(pie(data))
     .enter().append("g")
-      .attr("class", "arc2");
+    .attr("class", "arc2");
 
    // append path
   g2.append("path")
-      .attr("d", arc2)
-      .style("fill", function(d) { return color(d.data.fruit); })
+    .attr("d", arc2)
+    .style("fill", function(d) { return color(d.data.fruit); })
     .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .attrTween("d", tweenDonut);
+    .ease(d3.easeLinear)
+    .duration(2000)
+    .attrTween("d", tweenDonut);
 
    // append text
   g2.append("text")
     .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
+    .ease(d3.easeLinear)
+    .duration(2000)
     .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.fruit; });
+    .attr("dy", ".35em")
+    .text(function(d) { return d.data.fruit; });
 
 });
 
